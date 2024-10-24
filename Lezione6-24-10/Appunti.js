c346737309355db69d7f3f185254ffdb46544695
Raffaele Melluso <raffaele.melluso@edu.unito.it>
2024-10-24 10:53:11 +0200

    yes

--- Lezione6-24-10/Appunti.js
+++ Lezione6-24-10/Appunti.js
@@ -22,3 +22,39 @@ function extractFormData(form) {
 }
 /**usiamo axios*/
 /**codice per il submitForm*/
+
+/*se ho*/
+//{"firstName":"Mario","lastName":"Rossi","email":"}
+
+/**ma ho */
+//{'{"firstname":"Mario","lastName":"Rossi","email":"}'}
+
+/*in ajax, vuol dire che ho fatto un json del json*/
+
+/**
+ * AXIOS
+ * è una libreria da includere su html
+ * permette di gestire i dati tramite get e post
+ */
+axios.get('/user')
+.then(res => {
+    console.log("CIAO")
+})
+.catch(err => {
+    console.error(err); 
+})
+.then({/*codice da eseguire comunque*/});
+
+function sendAxiosQuery() {
+    axios.post('/user', {
+        firstName: 'Mario',
+        lastName: 'Rossi',
+        email: 'hello@hello.it'
+    });
+}
+/** Axios nel lato del post controlla in automatico se ci sono errori
+ *  JS fetch invece non lo fa, devi controllare tu a mano
+ *  Axios è supportato da tutti i browser
+ *  Noi usiamo solo axios per fare le chiamate durante il corso
+ *  Possiamo usare le promises anche con axios
+ */
